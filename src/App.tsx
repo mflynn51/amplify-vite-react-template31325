8d1f90eff055ae3bc27f006e2ce5a0c5abdd74e4
mflynn51 <59885949+mflynn51@users.noreply.github.com>
2025-03-13 21:28:38 +0000

    boned

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const mapCenter: [number, number] = [51.505, -0.09]; // London
   const mapZoom = 13;
+  const circleCenter: [number, number] = [51.505, -0.12]; // Example circle center
+  const circleRadius = 500; // Example circle radius (meters)
+
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -37,7 +40,12 @@ function App() {
           Review next step of this tutorial.
         </a>
         <div className="App">
-          <LeafletMap center={mapCenter} zoom={mapZoom} />
+          <LeafletMap 
+          center={mapCenter} 
+          zoom={mapZoom} 
+          circleCenter={circleCenter}
+          circleRadius={circleRadius}
+        />
         </div>
       </div>
     </main>
